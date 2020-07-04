f06846a92fc09b198d8e13e20045e585615099eb
david lerner <dmlerner@gmail.com>
2020-07-04 08:52:48 -0700

    chromecast unbroken

--- papmonitor/alarm/chromecast.py
+++ papmonitor/alarm/chromecast.py
@@ -2,41 +2,48 @@ import time
 import os
 import pychromecast 
 import contextlib
+import wslprocess
 
 import logging
-logger = logging.getLogger(__name__)
+print(__name__)
 
 class ChromeCast:
 
     @staticmethod
-    def or_reset(f):
-        def _f(*args, **kwargs):
-            try:
-                return f(*args, **kwargs)
-            except:
-                self._chromecast = None
-        return _f
+    def get_by_name(name):
+        chromecasts = pychromecast.get_chromecasts()
+        print('found chromecasts: %s',
+                [c.device.friendly_name for c in chromecasts])
+        return next(cc for cc in chromecasts if cc.device.friendly_name == name)
 
     def __init__(self, name):
         self.name = name
-        self._chromecast = Chromecast.get_by_name(name)
-        self.ip = self._chromecast.host
-        self.pause = self._chromecast.media_controller.pause
-        self.set_volume_muted = self._chromecast.set_volume_muted
-        self.set_volume = self._chromecast.set_volume
-        self.status = self._chromecast.status
+        self._chromecast = None
+        self.vlc_pid = None # Windows side PID
+
+    def ip(self):
+        return self.get_chromecast().host
+
+    def pause(self):
+        return self.get_chromecast().media_controller.pause()
+
+    def mute(self):
+        return self.get_chromecast().set_volume_muted(True)
+
+    def unmute(self):
+        return self.get_chromecast().set_volume_muted(False)
+
+    def set_volume(self): # float [0,1]
+        return self.get_chromecast().set_volume()
+
+    def status(self):
+        return self.get_chromecast().status()
 
     def get_chromecast(self):
         if self._chromecast is None:
-            self._chromecast = ChromeCast.get_by_name(self.chromecast_name)
+            self._chromecast = ChromeCast.get_by_name(self.name)
+        assert self._chromecast
         return self._chromecast
-
-    @staticmethod
-    def get_by_name(name):
-        chromecasts = pychromecast.get_chromecasts()
-        logger.debug('found chromecasts: %s',
-                [c.device.friendly_name for c in chromecasts])
-        return next(cc for cc in chromecasts if cc.device.friendly_name == name)
 
     @staticmethod
     def get_player_path():
@@ -51,7 +58,7 @@ class ChromeCast:
                 '--play-and-exit',
                 '--sout="#chromecast"',
                 '--demux-filter=demux_chromecast',
-                f'--sout-chromecast-ip={self.ip}',
+                f'--sout-chromecast-ip={self.ip()}',
                 #'>logs 2>&1', # TODO: handle logging
                 )
 
@@ -60,9 +67,8 @@ class ChromeCast:
         flags = self.get_play_flags()
         return f'{player_path} {" ".join(flags)} {media_path} &'
 
-    @or_reset
     def play(self, media_path):
-        logger.info('play %s', media_path)
+        print('play %s', media_path)
         command = self.get_play_command(media_path)
 
         self._chromecast.wait() # TODO: needed? move to init?
@@ -72,40 +78,45 @@ class ChromeCast:
         # print(mc.status)
 
         # Needed to avoid connection noise
-        logger.debug('muting')
-        self.set_volume_muted(True)
+        print('muting')
+        self.mute()
         time.sleep(5)
-        logger.info(command)
+        print(command)
+        existing_vlcs = wslprocess.get_pids('vlc')
         os.system(command)
         time.sleep(5)
-        logger.debug('unmuting')
-        self.set_volume_muted(False)
+        self.vlc_pid = (wslprocess.get_pids('vlc') - existing_vlcs).pop()
+        print('unmuting')
+        self.unmute()
     
-    @or_reset
     def stop(self):
-        logger.info('')
+        print('')
         self._chromecast.media_controller.stop()
 
-    @or_reset
     def unpause(self):
-        logger.info('')
+        print('')
         self._chromecast.media_controller.play()
 
-    @or_reset
     def close(self):
-        logger.info('')
+        print('')
         self.stop()
         self._chromecast.disconnect()
+        wslprocess.kill_pids([self.vlc_pid])
+
+    def is_open(self):
+        return self.vlc_pid in wslprocess.getpids('vlc')
 
 
 def main():
     global c, cc, mc
     with contextlib.closing(ChromeCast('bedroom speaker')) as c:
-        cc = c._chromecast
+        cc = c.get_chromecast()
         mc = cc.media_controller
         #c = ChromeCast('Gym')
         print('play')
         filename = 'file:///C:/Users/david/google-drive/coding/papmonitor/papmonitor/alarm/audio.mp3'
+        #import pdb; pdb.set_trace()
+
         c.play(filename)
         print('pause')
         c.pause()
